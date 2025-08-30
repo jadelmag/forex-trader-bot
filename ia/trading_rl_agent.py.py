878580649e31a4f3e3c4f44574dfec0793089282
jadelmag <kientienemibarraespaciadora@gmail.com>
2025-08-30 15:02:36 +0200

    updated requeriments.txt

--- ia/trading_rl_agent.py.py
+++ ia/trading_rl_agent.py.py
@@ -1,7 +1,7 @@
 # ia/trading_rl_agent.py
 
-import gym
-from gym import spaces
+import gymnasium as gym
+from gymnasium import spaces
 import numpy as np
 import pandas as pd
 from stable_baselines3 import PPO
